4cbd99ff89fd620b4f3a578f4b14095f41797070
James Nascimento <james.nascimento94@yahoo.com>
2024-05-22 02:48:42 +0000

    finished helmet.js challenges

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
@@ -10,14 +11,31 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    try {
+        console.log(hash)
+    } catch {
+        console.log(err);
+    }
+    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    try {
+        console.log(res) //true
+    } catch {
+        console.log(err);
+    }
 
+    });
+  });
 
 
 //END_ASYNC
 
 //START_SYNC
-
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
